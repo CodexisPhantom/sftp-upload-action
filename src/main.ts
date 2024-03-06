9ed324b09bd92dfe7301d78858b066e8a62c3d12
CodexisPhantom <gabdostuni47400@gmail.com>
2024-03-06 21:06:53 +0100

    feat: initial roleplay dep

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import { debug, getBooleanInput, getInput, setFailed, warning } from "@actions/core"
 import * as Client from "ssh2-sftp-client"
 import Upload from "./types/Upload"
-import minimatch, { MinimatchOptions } from "minimatch"
 import pLimit from "p-limit"
+import axios from "axios"
 import * as fs from "fs"
-
-const minimatch_options: MinimatchOptions = {
-	dot: true,
-	matchBase: true
-}
 
 /** 
  * Converts a list of uploads as as string into an array of objects
@@ -20,12 +15,12 @@ function parse_uploads(uploads_str: string): Upload[] {
 	const data: string[] = uploads_str.split("\n").filter(d => d != "")
 	const uploads: Upload[] = []
 
-	if(data.length === 0)
+	if (data.length === 0)
 		throw new Error("No uploads were defined, please ensure you enter destinations to upload your files!")
 
 	data.forEach(upload => {
 		const [from, to] = upload.trim().split("=>")
-		if(!from || !to)
+		if (!from || !to)
 			return
 		uploads.push({
 			from: from.trim(),
@@ -36,51 +31,39 @@ function parse_uploads(uploads_str: string): Upload[] {
 }
 
 // returns a list of glob patterns from a string
-function parse_ignored(ignored_str: string): string[] {
+function parse_delete_folders(ignored_str: string): string[] {
 	return ignored_str.trim().split("\n").map(Function.prototype.call, String.prototype.trim).filter(e => e != "")
 }
 
-// determines whether a file should be uploaded
-function is_uploadable(file: string, patterns: string[]){
-	return !patterns.some(p => minimatch(file, p, minimatch_options))
-}
-
 // recursively delete a remote folder
-async function delete_folder(sftp: Client, dir: string){
+async function delete_folder(sftp: Client, dir: string) {
 	debug(`Deleting existing files for ${dir}...`)
-	try{
+	try {
 		await sftp.rmdir(dir, true)
 		debug(`${dir} has been deleted.`)
-	}catch(e: any){
+	} catch (e: any) {
 		warning(`Unable to delete existing files for ${dir} before upload. ${e}`)
 	}
 }
 
 // read a file and convert it to a string. Returns an empty string if the path is empty.
-function file_to_string(path: string){
-	if(path === ""){
+function file_to_string(path: string) {
+	if (path === "") {
 		return ""
 	}
-	return fs.readFileSync(path, {encoding: "utf8"})
+	return fs.readFileSync(path, { encoding: "utf8" })
 }
 
-async function main(sftp: Client){
+async function main(sftp: Client) {
 	try {
+		const port: number = +getInput("port")
 		const server: string = getInput("server")
 		const username: string = getInput("username")
 		const password: string = getInput("password")
-		const passphrase: string = getInput("passphrase")
-		const key: string= getInput("key")
-		const port: number = +getInput("port")
 		const isDryRun: boolean = getBooleanInput("dry-run")
 		const uploads: Upload[] = parse_uploads(getInput("uploads"))
-		const ignored: string[] = parse_ignored(getInput("ignore"))
-		const shouldDelete: boolean = getBooleanInput("delete")
-
-		// attempt to read ignored files from a file, if it is not defined directly
-		if(ignored.length == 0){
-			ignored.push(...parse_ignored(file_to_string(getInput("ignore-from"))))
-		}
+		const delete_folders: string[] = parse_delete_folders(getInput("delete_folders"))
+		const pterodactyl_api_key: string = getInput("pterodactyl_api_key")
 
 		debug(`Connecting to ${server} as ${username} on port ${port}`)
 
@@ -89,50 +72,97 @@ async function main(sftp: Client){
 			username: username,
 			password: password,
 			port: port,
-			privateKey: key,
-			passphrase: passphrase,
 		})
 
 		// allow only 1 sftp operation to occur at once
 		const limit = pLimit(1)
 		const promises: Promise<string | void>[] = []
 
-		if(shouldDelete && !isDryRun){
+		if (!isDryRun) {
 			debug("Deleting folders...")
-			for(const upload of uploads) {
-				promises.push(limit(() => delete_folder(sftp, upload.to)))
+
+			try {
+				await axios({
+					method: "post",
+					url: "https://my.eralive.fr/api/client/servers/26be620d/files/delete",
+					headers: {
+						"Accept": "application/json",
+						"Content-Type": "application/json",
+						"Authorization": "Bearer " + pterodactyl_api_key,
+					},
+					data: {
+						"root": "/",
+						"files": delete_folders,
+					}
+				})
+			} catch (error: any) {
+				setFailed(error.message)
 			}
+
 			await Promise.allSettled(promises)
-			promises.splice(0,promises.length)
+			promises.splice(0, promises.length)
 		}
 
 		debug("Preparing upload...")
-		for(const upload of uploads) {
+
+		for (const upload of uploads) {
 			debug(`Processing ${upload.from} to ${upload.to}`)
+
 			promises.push(limit(() => sftp.uploadDir(upload.from, upload.to, {
 				filter: file => {
-					if(is_uploadable(file, ignored)){
-						if(isDryRun){
-							console.log(`${file} would have been uploaded`)
-							return false
-						}else{
-							debug(`Uploading ${file}`)
-							return true
-						}
+					if (isDryRun) {
+						console.log(`${file} would have been uploaded`)
+						return false
+					} else {
+						debug(`Uploading ${file}`)
+						return true
 					}
-					debug(`Skipping ${file}`)
-					return false
 				}
 			})))
 		}
+
 		await Promise.allSettled(promises)
 		debug("Upload process complete.")
+
 		await sftp.end()
 		debug("Session ended.")
 
+		try {
+			await axios({
+				method: "post",
+				url: "https://pterodactyl.file.properties/api/client/servers/1a7ce997/power",
+				headers: {
+					"Accept": "application/json",
+					"Content-Type": "application/json",
+					"Authorization": "Bearer " + pterodactyl_api_key,
+				},
+				data: {
+					"signal": "kill",
+				}
+			})
+		} catch (error: any) {
+			setFailed(error.message)
+		}
+
+		try {
+			await axios({
+				method: "post",
+				url: "https://pterodactyl.file.properties/api/client/servers/1a7ce997/power",
+				headers: {
+					"Accept": "application/json",
+					"Content-Type": "application/json",
+					"Authorization": "Bearer " + pterodactyl_api_key,
+				},
+				data: {
+					"signal": "start",
+				}
+			})
+		} catch (error: any) {
+			setFailed(error.message)
+		}
 	} catch (error: any) {
 		setFailed(error.message)
 	}
 }
 
-export { main, parse_uploads, parse_ignored, is_uploadable, delete_folder, file_to_string }
+export { main, parse_uploads, delete_folder, file_to_string }
